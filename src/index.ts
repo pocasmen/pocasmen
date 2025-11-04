c67a79750af47ed4dbb40e16dd62bfe310c8dd94
User <user@example.com>
2025-11-04 22:38:56 +0000

    Substitui express-list-endpoints por debug manual de rotas

--- src/index.ts
+++ src/index.ts
@@ -299,10 +299,27 @@ app.post('/schedules', async (req, res) => {
   }
 });
 
-// Endpoint de Debug para listar todas as rotas
-import listEndpoints from 'express-list-endpoints';
+// Endpoint de Debug para listar todas as rotas (manual)
 app.get('/debug/routes', (req, res) => {
-  res.json(listEndpoints(app));
+  const routes: any[] = [];
+  app._router.stack.forEach((middleware: any) => {
+    if (middleware.route) { // Rotas diretas
+      routes.push({
+        path: middleware.route.path,
+        method: Object.keys(middleware.route.methods)[0].toUpperCase()
+      });
+    } else if (middleware.name === 'router') { // Sub-routers
+      middleware.handle.stack.forEach((handler: any) => {
+        if (handler.route) {
+          routes.push({
+            path: handler.route.path,
+            method: Object.keys(handler.route.methods)[0].toUpperCase()
+          });
+        }
+      });
+    }
+  });
+  res.json(routes);
 });
 
 
